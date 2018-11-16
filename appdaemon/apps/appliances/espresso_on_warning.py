a72d7b9e0f9e5a30ce4e31fbf9f896fd16c40548
Aephir <waldenb@gmail.com>
2018-11-16 10:13:39 +0100

    Changed timing on espresso notifications

--- appdaemon/apps/appliances/espresso_on_warning.py
+++ appdaemon/apps/appliances/espresso_on_warning.py
@@ -1,4 +1,6 @@
-# Warn me if the espresso machine is left "on" for more than 1 hour.
+# Warn me if the espresso machine is left "on" for more than 45 minutes.
+# Again if it is still on 3after 1 hour.
+# After 1.5 hours, turn off and notify.
 
 import appdaemon.plugins.hass.hassapi as hass
 import time
@@ -8,44 +10,30 @@ class EspressoStatus(hass.Hass):
 
     def initialize(self):
         # Run SendNotification function if switch.switch has been on for 1 hour.
-        self.listen_state(self.SendNotification0,"switch.switch", new="on", duration=3600) #3600
+        self.listen_state(self.SendNotification0,"switch.switch", new="on", duration=2700) #2700 seconds = 0.75h
 
         # Run SendNotification function if switch.switch has been on for 1.5 hours.
-        self.listen_state(self.SendNotification1,"switch.switch", new="on", duration=5400) #5400
+        self.listen_state(self.SendNotification1,"switch.switch", new="on", duration=3600) #3600 seconds = 1h
 
         # Run SendNotification function if switch.switch has been on for 2 hours.
-        self.listen_state(self.SendNotification2,"switch.switch", new="on", duration=7200) #7200
+        self.listen_state(self.SendNotification2,"switch.switch", new="on", duration=5400) #5400 seconds = 1.5h
 
 
     # Send a notification to telegram
     def SendNotification0(self, entity, attribute, old, new, kwargs):
         # if self.get_state("switch.switch") == "on":
-        self.call_service("notify/home_aephir_bot", message="Espresso machine has been on for 1 hour", data={"inline_keyboard":"Turn Off:/espresso_off, I Know:/removekeyboard"})
-        self.log("Espresso machine has been on for 1 hour")
-
-# [[["Turn off", "/espresso_off"], ["I know", "/removekeyboard"]]]
+        self.call_service("notify/home_aephir_bot", message="Espresso machine has been on for 45 mins", data={"inline_keyboard":"Turn Off:/espresso_off, I Know:/removekeyboard"})
+        self.log("Espresso machine has been on for 45 mins")
 
     # Send a notification to telegram
     def SendNotification1(self, entity, attribute, old, new, kwargs):
         # if self.get_state("switch.switch") == "on":
-        self.call_service("notify/home_aephir_bot", message="Espresso machine has been on for 1.5 hours", data={"inline_keyboard":"Turn off:/espresso_off, No action:/removekeyboard"})
-        self.log("Espresso machine has been on for 1.5 hours")
-
+        self.call_service("notify/home_aephir_bot", message="Espresso machine has been on for 1 hour", data={"inline_keyboard":"Turn off:/espresso_off, No action:/removekeyboard"})
+        self.log("Espresso machine has been on for 1 hour")
 
     # Send a notification to telegram
     def SendNotification2(self, entity, attribute, old, new, kwargs):
         # if self.get_state("switch.switch") == "on":
-        self.call_service("notify/home_aephir_bot", message="Espresso machine has been on for 2 hours. I'm turning it off", data={"inline_keyboard":"Turn back on:/espresso_on, OK:/removekeyboard"})
+        self.call_service("notify/home_aephir_bot", message="Espresso machine has been on for 1.5 hours. I'm turning it off", data={"inline_keyboard":"Turn back on:/espresso_on, OK:/removekeyboard"})
         self.turn_off("switch.switch")
-        self.log("Espresso machine has been on for 2 hours. Turning off")
-
-'''
-    def initialize(self):
-        self.listen_event(self.telegram_callback_cb, "telegram_callback")
-        self.call_service('telegram_bot/send_message', target = 111111, message = 'Try me', inline_keyboard = [[("Yes", "/yes"),("No", "/no")],  [("Maybe", "/maybe")]])
-
-    def telegram_callback_cb(self, type, payload_event, kwargs):
-        callback = payload_event["data"]
-        user = payload_event["from_first"]
-        self.log('Got "{}" from {}'.format(callback, user))
-'''
+        self.log("Espresso machine has been on for 1.5 hours. Turning off")
