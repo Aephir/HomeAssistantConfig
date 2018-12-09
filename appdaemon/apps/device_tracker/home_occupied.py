17fcfe7376d4ba3b3adaef568865c82d2841b702
Aephir <waldenb@gmail.com>
2018-12-10 00:26:36 +0100

    Created occupancy sensor

--- appdaemon/apps/device_tracker/home_occupied.py
+++ appdaemon/apps/device_tracker/home_occupied.py
@@ -8,22 +8,56 @@ import time
 class HomeOccupancy(hass.Hass):
 
     def initialize(self):
-        self.listen(self.occupancy,"input_boolean.aephirhome360")
-        self.listen(self.occupancy,"input_boolean.kristinahome360")
-        self.listen(self.occupancy,"input_boolean.emiliehome360")
-        self.listen(self.occupancy,"input_boolean.naiahome360")
-        self.listen(self.occupancy,"input_boolean.guest_mode")
+
+        self.deviceTrackers = [
+            'device_tracker.meta_walden',
+            'device_tracker.meta_kristina'
+            # 'device_tracker.meta_emilie',
+            # 'device_tracker.meta_naia',
+            ]
+
+        self.friendlyName = {
+            'device_tracker.meta_walden': 'Walden',
+            'device_tracker.meta_kristina': 'Kristina',
+            'device_tracker.meta_emilie': 'Emilie',
+            'device_tracker.meta_naia': 'Naia',
+            'input_boolean.guest_mode': 'Guest'
+            }
+
+        self.anyHome = []
+
+        self.occupancy(None)
+
+        for entity in self.deviceTrackers:
+            self.listen_state(self.occupancy, entity)
 
 
-    def whereIs(self, entity_id):
-        return self.get_state(entity_id)
-
-    def occupancy(self, entity_id):
-        aephir = ''
-        kristina = ''
-        emilie = ''
-        naia = ''
+    def occupancy(self, entity):
         guest_mode = ''
+        newStatus = ''
+        numberOfPeople = 0
+
+        self.anyHome = []
+
+        for entity in self.deviceTrackers:
+            if self.get_state(entity) == "home":
+                self.anyHome.append(self.friendlyName[entity])
+                numberOfPeople += 1
+                newStatus = "Occupied"
+            elif self.get_state('input_boolean.guest_mode') == 'on':
+                newStatus = "Occupied"
+            else:
+                newStatus = "Unoccupied"
+
+        if self.get_state('input_boolean.guest_mode') == "on":
+            guest_mode == "on"
+        else:
+            guest_mode == "off"
 
 
-    def 
+        self.set_state('sensor.occupancy', state = newStatus, attributes = {
+            'friendly_name': "Home occupancy",
+            'number_of_people': numberOfPeople,
+            'guests': guest_mode,
+            'who_is_home': self.anyHome
+            })
