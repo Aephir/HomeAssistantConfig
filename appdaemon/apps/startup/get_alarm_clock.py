53b81243d2e17066db749b2796e69e7034493f8a
Aephir <waldenb@gmail.com>
2019-09-27 17:26:29 +0200

    routine update

--- appdaemon/apps/startup/get_alarm_clock.py
+++ appdaemon/apps/startup/get_alarm_clock.py
@@ -4,17 +4,7 @@ class Alarm(hass.Hass):
 
     def initialize(self):
 
-        # message = 'https://autoremotejoaomgcd.appspot.com/sendmessage?key=APA91bHdG9gIIRophSN4rAlk4Y23LJa9o4KH_Q4I44Fzzf3luTHuOITsn0wOwZg3OhfdiPYsjYgWST8ckLapuJPjcHmY4y9G1cHOW96otuGr4M-irV57GXOHP4dC9QVNGcDOckZk2A45&message=sensor.alarm_clock=:=get'
-        requests.get('https://autoremotejoaomgcd.appspot.com/sendmessage?key=APA91bHdG9gIIRophSN4rAlk4Y23LJa9o4KH_Q4I44Fzzf3luTHuOITsn0wOwZg3OhfdiPYsjYgWST8ckLapuJPjcHmY4y9G1cHOW96otuGr4M-irV57GXOHP4dC9QVNGcDOckZk2A45&message=get_alarm=:=get')
+        requests.get(self.args['autoremote_url'] + 'get_alarm=:=get')
 
-        # requests.get(message)
-
-    #     self.listen_state(self.send_request, 'input_boolean.vacation_mode')
-    #
-    # def send_request(self, entity, attribute, old, new, kwargs):
-    #
-    #     self.log('This is a test for alarm')
-    #
-    #     message = 'https://autoremotejoaomgcd.appspot.com/sendmessage?key=APA91bHdG9gIIRophSN4rAlk4Y23LJa9o4KH_Q4I44Fzzf3luTHuOITsn0wOwZg3OhfdiPYsjYgWST8ckLapuJPjcHmY4y9G1cHOW96otuGr4M-irV57GXOHP4dC9QVNGcDOckZk2A45&message=sensor.alarm_clock=:=get'
-    #
-    #     requests.get(message)
+# Dummy app config only with secrets.
+# self.app_config['name_of_dummy_app']['secret_thing']
--- appdaemon/apps/test/test.py
+++ appdaemon/apps/test/test.py
@@ -1,36 +1,42 @@
 # Notify upon problems with plants
 
 import appdaemon.plugins.hass.hassapi as hass
-import datetime
-import time
-
 
 class Test(hass.Hass):
 
     def initialize(self):
 
-        self.listen_event(self.receive_telegram_callback, 'telegram_callback')
+        # self.log(self.args["test"])
 
-    def receive_telegram_callback(self, event_id, payload_event, *args):
+        self.listen_state(self.test_app, 'input_boolean.vacation_mode')
 
-        data_callback   = payload_event['data']
-        callback_id     = payload_event['id']
-        message_id      = payload_event['message_id']
-        chat_id         = payload_event['user_id']
+    def test_app(self, entity, attribute, old, new, kwargs):
 
-        self.log(payload_event)
+        self.log(self.args["test_secret"])
+        self.log('TEST')
 
-        if payload_event['data'] == '/removekeyboard':
+        # self.listen_event(self.receive_telegram_callback, 'telegram_callback')
 
-            self.call_service(
-                'telegram_bot/answer_callback_query',
-                message='OK',
-                callback_query_id=callback_id
-                )
-
-            self.call_service(
-                'telegram_bot.edit_replymarkup',
-                message_id=message_id,
-                chat_id=user_id,
-                inline_keyboard=[]
-                )
+    # def receive_telegram_callback(self, event_id, payload_event, *args):
+    #
+    #     data_callback   = payload_event['data']
+    #     callback_id     = payload_event['id']
+    #     message_id      = payload_event['message_id']
+    #     chat_id         = payload_event['user_id']
+    #
+    #     self.log(payload_event)
+    #
+    #     if payload_event['data'] == '/removekeyboard':
+    #
+    #         self.call_service(
+    #             'telegram_bot/answer_callback_query',
+    #             message='OK',
+    #             callback_query_id=callback_id
+    #             )
+    #
+    #         self.call_service(
+    #             'telegram_bot.edit_replymarkup',
+    #             message_id=message_id,
+    #             chat_id=user_id,
+    #             inline_keyboard=[]
+    #             )
