04b7f0ebb6b6716e020af53a6a238bec66065df6
Aephir <waldenb@gmail.com>
2019-09-28 17:09:30 +0200

    printer

--- appdaemon/apps/startup/get_alarm_clock.py
+++ appdaemon/apps/startup/get_alarm_clock.py
@@ -5,9 +5,3 @@ class Alarm(hass.Hass):
     def initialize(self):
 
         requests.get(self.args['autoremote_url'] + 'get_alarm=:=get')
-
-
-
-
-# Dummy app config only with secrets.
-# self.app_config['name_of_dummy_app']['secret_thing']
