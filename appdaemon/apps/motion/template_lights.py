5f24bee645aada50da48ad504921e194ad6ad53d
Aephir <waldenb@gmail.com>
2019-10-07 22:23:05 +0200

    fixed "awake.py"

--- appdaemon/apps/motion/template_lights.py
+++ appdaemon/apps/motion/template_lights.py
@@ -1,161 +1,80 @@
 """
-Motion sensors to control the main floor kitchen lights.
+Template
+Control X light from a number of motion sensors, illumination sensors, etc.
 """
 
 import appdaemon.plugins.hass.hassapi as hass
 
-# Global apps/functions to use/import. Need to figure out how!!
-#   > workday_when_waking (uses workday sensor, vacaton mode sensor, pre-post midnight)
-#   > cooking (uses cooking mode and time of day, both morning and evening)
-#   >
-#
-
-def toInt(inString):
-    try:
-        return int(float(inString))
-    except ValueError:
-        return 0
 
 class MotionClass(hass.Hass):
 
     def initialize(self):
-        """ Initializes and listens for state changes in motion sensor"""
 
-        # list of lights that are turned on by the motion.
-        self.lights                 = [
-            'light.kitchen_spots',
-            'light.kitchen_cabinet_lights'
+        self.motion_sensors = [
+            'binary_sensor.presence_top_floor_stairway',    # Top floor stairway
+            'binary_sensor.presence_top_floor_tv_room'      # Top floor tv room
             ]
 
-        # list of motion sensors that trigger the automation.
-        self.motion_sensors         = [
-            'binary_sensor.presence_kitchen'
-            ]
-
-        # list of illumination sensors
         self.illumination_sensors   = [
-            'sensor.lightlevel_kitchen'
-            ]
-        
-        # list of input_booleans
-        self.input_booleans         = [
-            'input_boolean.cooking_mode'
+            'sensor.lightlevel_top_floor_stairway'
             ]
 
-        self.timer = None
+        self.lights         = [
+            'light.top_floor_hallway'
+            ]
 
         for entity in self.motion_sensors:
-            self.listen_state(self.motionTrigger, entity) # motion sensors
+            self.listen_state(self.motion_trigger,entity)
 
-        self.listen_state(self.motionTrigger,'switch.switch')
+        self.timer = None # Set timer off to also reset if otther motion sensor is turned on?
 
-        self.listen_state(self.inputBoolean,"input_boolean.main_floor_lights_motion_control")
-        self.listen_state(self.motionTrigger,"input_boolean.cooking_mode")
-
-    def awake(self):
-        work_next_morning   = self.now_is_between("12:00:00", "00:00:00") and
-            self.get_state('binary_sensor.workday_tomorrow') == 'on' or
-            self.now_is_between("00:00:00", "08:00:00") and
-            self.get_state('binary_sensor.workday_today') == 'on'
-            
-        self.get_state
-
-    def lightsOff(self, kwargs):
-        """ iterates through lights and turns them off"""
-        for entity in self.light_entity_ids:
-            self.turn_off(entity)
-        self.cancel_timer(self.timer)
-
-    def lightsOn(self, illumination):
-        """ Turns the lights on, depending on time and awake state"""
-
-        cooking = self.cooking()
-        dark = int(illumination) < 80 # 40?? 50??
-        party_mode = self.get_state('input_boolean.party_mode') == 'on'
-
-        if party_mode:
-            self.turn_on("light.kitchen_lights",brightness=255,kelvin=2700)
-
-        elif self.now_is_between("07:00:00", "21:00:00"):
-            if dark:
-                if cooking:
-                    for entity in self.light_entity_ids:
-                        self.turn_on(entity, brightness=255, kelvin=2700)
-                        # self.call_service('remote/send_command', entity_id = 'remote.kitchen_remote', command = 'fume_hood_lights')
-                elif self.get_state('switch.switch') == 'on':
-                    self.turn_on('light.kitchen_spots',brightness=255, kelvin=2700)
-                    self.turn_on('light.kitchen_cabinet_light_2', brightness=255, kelvin=2700)
-                else:
-                    self.turn_on('light.kitchen_spots',brightness=255, kelvin=2700)
-            else:
-                if cooking:
-                    self.turn_on('light.kitchen_cabinet_lights', brightness=255, kelvin=2700)
-                        # self.call_service('remote/send_command', entity_id = 'remote.kitchen_remote', command = 'fume_hood_lights')
-                elif self.get_state('switch.switch') == 'on':
-                    self.turn_on('light.kitchen_cabinet_light_2', brightness=255, kelvin=2700)
-        elif self.now_is_between("21:00:00", "22:00:00"):
-            if dark:
-                if self.get_state('switch.switch') == 'on':
-                    self.turn_on('light.kitchen_spots',brightness=255, kelvin=2700)
-                    self.turn_on('light.kitchen_cabinet_light_2', brightness=255, kelvin=2700)
-                elif any([ self.isOn(entity_id) for entity_id in ['light.dining_table_lights', 'light.conservatory_lights']]):
-                    self.turn_on('light.kitchen_spots', brightness=255, kelvin=2700)
-                else:
-                    self.turn_on('light.kitchen_spots', brightness=10, kelvin=2200)
-            else:
-                if self.get_state('switch.switch') == 'on':
-                    self.turn_on('light.kitchen_cabinet_light_2', brightness=255, kelvin=2700)
-                elif any([ self.isOn(entity_id) for entity_id in ['light.dining_table_lights', 'light.conservatory_lights']]):
-                    self.turn_on('light.kitchen_spots', brightness=255, kelvin=2700)
-                else:
-                    self.turn_on('light.kitchen_spots', brightness=10, kelvin=2200)
-        elif self.now_is_between("22:00:00", "07:00:00"): # everything between 22 and 7
-            if dark:
-                if self.get_state('switch.switch') == 'on':
-                    self.turn_on('light.kitchen_spots',brightness=255, kelvin=2700)
-                    self.turn_on('light.kitchen_cabinet_light_2', brightness=255, kelvin=2700)
-                elif self.isOn('light.dining_table_lights'):
-                    self.turn_on('light.kitchen_spots', brightness=255, kelvin=2200)
-                else:
-                    self.turn_on('light.kitchen_spots', brightness=10, kelvin=2200)
-            else:
-                if self.get_state('switch.switch') == 'on':
-                    self.turn_on('light.kitchen_cabinet_light_2', brightness=255, kelvin=2700)
-                elif self.isOn('light.dining_table_lights'):
-                    self.turn_on('light.kitchen_spots', brightness=255, kelvin=2200)
-                else:
-                    self.turn_on('light.kitchen_spots', brightness=10, kelvin=2200)
+        self.listen_state(self.input_boolean,"input_boolean.top_floor_lights_motion_control")
 
 
-    def motionTrigger(self, entity, attribute, old, new, kwargs):
+    def get_integer_state(self, entity_id):
+        try:
+            return int(self.get_state(entity_id))
+        except ValueError:
+            return 0
+
+    def motion_trigger(self, entity, attribute, old, new, kwargs):
         """
         On motion: Turn off timer (if running), and turn on lights.
         On motion off: Start timer, turn off lights after 5 minutes.
         """
 
-        if self.timer != None and entity != 'switch.switch':
+        party_mode = self.get_state('input_boolean.party_mode') == 'on'
+
+        if party_mode:
             self.cancel_timer(self.timer)
-
-        if self.get_state('binary_sensor.presence_kitchen') == 'on': # if we got motion.
-            illumination = max([toInt(self.get_state(entity_id)) for entity_id in self.illumination_sensors ])
+            self.turn_on(light, brightness=255,kelvin=2700)
+        elif new == 'on' and entity in self.motion_sensors:
+            illumination = max([self.get_integer_state(entity_id) for entity_id in self.illumination_sensors])
             self.cancel_timer(self.timer)
-            self.lightsOn(illumination)
-        elif self.get_state('binary_sensor.presence_kitchen') == 'off': # we got no motion.
-            self.timer = self.run_in(self.lightsOff, 90)
+            if illumination < 500:
+                for light in self.lights:
+                    if self.get_state(light) == 'off': # If more, use for i in self.lights:  self.turn_on(i)
+                        self.lights_on(light)
+        elif new == 'off' and entity in self.motion_sensors:
+            if all([self.get_state(entity_id) == 'off' for entity_id in self.motion_sensors]):
+                self.timer = self.run_in(self.lights_off, 3)
 
 
-    def switchOff(self, entity, attribute, old, new, kwargs):
-        """
-        If the espresso machine is switched off and there's no motion in the kitchen, turn off lights.
-        """
+    def lights_on(self, light):
 
-        if self.get_state('binary_sensor.presence_kitchen') == 'off': # we got no motion.
-            self.lightsOff()
+        if self.get_state('sensor.awake') == 'on':
+            self.turn_on(light, brightness=255,kelvin=2700)
+        else:
+            self.turn_on(light, brightness=50,kelvin=2200)
 
 
-    def inputBoolean(self, entity, attribute, old, new, kwargs):
+    def lights_off(self, kwargs):
+
+        for light in self.lights:
+            self.turn_off(light)
+
+
+    def input_boolean(self, entity, attribute, old, new, kwargs):
 
         if new == "on":
-            if self.get_state("binary_sensor.presence_kitchen") == "on":
-                illumination = max([ toInt(self.get_state(entity_id)) for entity_id in self.illumination_sensors ])
-                self.lightsOn(illumination)
+            self.motion_trigger(entity, attribute, old, new, kwargs)
--- appdaemon/apps/sensors/awake.py
+++ appdaemon/apps/sensors/awake.py
@@ -1,41 +1,14 @@
+"""
+Script to determine whether or not we are awake.
+It will only set us as "awake" if we are also home.
+It has a 15 minute delay from we are in bed to it thinks we are going to sleep.
+"""
+
 import appdaemon.plugins.hass.hassapi as hass
 
 class Awake(hass.Hass):
 
     def initialize(self):
-
-        # anyone_awake = 'off'
-        # who_is_awake = ''
-        # 
-        # if self.get_state('binary_sensor.workday_tomorrow') == 'on' and self.get_state('vacation_mode') == 'off':
-        #     night_time  = self.now_is_between("20:30:00", "00:00:00")
-        # elif self.get_state('binary_sensor.workday_today') == 'on' and self.get_state('vacation_mode') == 'off':
-        #     night_time  = self.now_is_between("00:00:00", "08:00:00")
-        # else:
-        #     night_time  = self.now_is_between("20:30:00", "09:00:00")
-        # 
-        # if night_time:
-        #     if self.get_state('device_tracker.meta_walden') == 'home' and self.get_state('binary_sensor.aephir_in_bed') == 'off':
-        #         who_is_awake   += 'Walden, '
-        #         anyone_awake    = 'on'
-        # 
-        #     if self.get_state('device_tracker.meta_kristina') == 'home' and self.get_state('binary_sensor.kristina_in_bed') == 'off':
-        #         who_is_awake   += 'Kristina, '
-        #         anyone_awake    = 'on'
-        # 
-        # else:
-        #     if self.get_state('device_tracker.meta_walden') == 'home':
-        #         who_is_awake   += 'Walden, '
-        #         anyone_awake    = 'on'
-        #     if self.get_state('device_tracker.meta_kristina') == 'home':
-        #         who_is_awake   += 'Kristina, '
-        #         anyone_awake    = 'on'
-        # 
-        # who_is_awake    = who_is_awake[:len(who_is_awake)-2]
-
-        # self.set_state('sensor.awake', state = anyone_awake, attributes = {
-        #     'who_is_awake': who_is_awake
-        # })
 
         # Set up timers like this?
         # https://github.com/ai3xbe/Home_Assistant_public/blob/4bc98b8ba75c22017e694367b46a89319e889e53/AppDaemon/presence.py
@@ -44,43 +17,56 @@ class Awake(hass.Hass):
         self.cancel_timer(self.aephir_timer)
         self.cancel_timer(self.kristina_timer)
 
-        in_bed      = [
+        in_bed          = [
             'binary_sensor.aephir_in_bed',
             'binary_sensor.kristina_in_bed'
             ]
-        
-        booleans    = [
+
+        booleans        = [
             'input_boolean.party_mode'
+            ]
+
+        meta_trackers   = [
+            'device_tracker.meta_walden',
+            'device_tracker.meta_kristina'
             ]
 
         for sensor in in_bed:
             self.listen_state(self.timer_func, sensor)
 
+        # for tracker in meta_trackers:
+        #     self.listen_state(self.trackers, tracker)
+
+    def trackers(self, entity, attribute, old, new, kwargs):
+
+        try:
+            self.get_state('sensor.awake')
+        except:
+            self.awake()
+
+
     def timer_func(self, entity, attribute, old, new, kwargs):
-        
+
         self.log("Timer func running")
-        
+
+        ### Get from "global_functions.py -> workday_when_waking"
+
+
+
         if self.get_state('input_boolean.vacation_mode') == 'off':
             self.log('One')
-            if self.now_is_between("10:30:00", "00:00:00"):
+            if self.now_is_between("20:30:00", "00:00:00"):
                 self.log('Two')
                 night_time = self.get_state('binary_sensor.workday_tomorrow') == 'on'
             elif self.now_is_between("00:00:00", "08:00:00"):
                 self.log('Three')
                 night_time = self.get_state('binary_sensor.workday_today') == 'on'
+            else:
+                night_time = False
         else:
             self.log('Four')
-            night_time  = self.now_is_between("20:30:00", "09:00:00")
-                    
-        # 
-        # if self.get_state('binary_sensor.workday_tomorrow') == 'on' and self.get_state('vacation_mode') == 'off':
-        #     night_time  = self.now_is_between("10:30:00", "00:00:00") # Change 10:30 to 20:30!
-        #     self.log("Number 1")
-        # elif self.get_state('binary_sensor.workday_today') == 'on' and self.get_state('vacation_mode') == 'off':
-        #     night_time  = self.now_is_between("00:00:00", "08:00:00")
-        # else:
-        #     night_time  = self.now_is_between("20:30:00", "09:00:00")
-            
+            night_time = self.now_is_between("20:30:00", "09:00:00")
+
         self.log("is it night time?")
         self.log(night_time)
 
@@ -93,10 +79,16 @@ class Awake(hass.Hass):
                 self.log('Kristina bed')
                 self.kristina_timer = self.run_in(self.awake, 5) #900
         else:
-            self.set_state('sensor.awake', state = 'off', attributes = {
-                'who_is_awake': ''})
+            who_is_home = ''
+            if self.get_state('device_tracker.meta_walden') == 'home':
+                who_is_home += 'Walden, '
+            if self.get_state('device_tracker.meta_kristina') == 'home':
+                who_is_home += 'Kristina, '
+            who_is_awake = who_is_awake[:len(who_is_awake)-2]
+            self.set_state('sensor.awake', state = 'on', attributes = {
+                'who_is_awake': who_is_awake})
 
-    def awake(self):
+    def awake(self, kwargs):
 
         anyone_awake = 'off'
         who_is_awake = ''
